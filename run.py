6e316e3fd01000e6a756edbd81338bb961df9b4c
Ben Plaut <bplaut@rnn.ist.berkeley.edu>
2025-02-26 09:25:18 -0800

    changed name of logging dir to have latent dim and dataset

--- run.py
+++ run.py
@@ -19,7 +19,7 @@ parser.add_argument('--config',  '-c',dest="filename", metavar='FILE'
                     ,help =  'path to the config file',default='configs/vae.yaml')
 parser.add_argument('-r', '--train_dataset', type=str, help='Dataset to use for training')
 parser.add_argument('-e', '--test_dataset', type=str, help='Dataset to use for testing')
-parser.add_argument('-d', '--latent_dim', type=str, help='Latent dimension of the model. If provided, it will override the value in the config file')
+parser.add_argument('-d', '--latent_dim', type=int, help='Latent dimension of the model. If provided, it will override the value in the config file')
 
 args = parser.parse_args()
 with open(args.filename, 'r') as file:
@@ -29,10 +29,10 @@ with open(args.filename, 'r') as file:
         print(exc)
 
 if args.latent_dim is not None:
-    config['model_params']['latent_dim'] = int(args.latent_dim)
+    config['model_params']['latent_dim'] = args.latent_dim
 
 tb_logger =  TensorBoardLogger(save_dir=config['logging_params']['save_dir'],
-                               name=config['model_params']['name'],)
+                               name=f"{config['model_params']['name']}_{config['model_params']['latent_dim']}_{args.train_dataset}",)
 
 # For reproducibility
 seed_everything(config['exp_params']['manual_seed'], True)
