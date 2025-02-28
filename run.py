4d4a089799095241520308b9df29236fd0b73cb3
Ben Plaut <bplaut@rnn.ist.berkeley.edu>
2025-02-27 18:16:06 -0800

    minor printing thing

--- run.py
+++ run.py
@@ -85,7 +85,7 @@ if args.trained_model_path is None:
 else:
     checkpoint_path = args.trained_model_path
 if args.test_dataset is not None:
-    print(f"----\nTesting {config['model_params']['name']} using checkpoint {args.trained_model_path}\n----")
+    print(f"----\nTesting {config['model_params']['name']} on {args.test_dataset}\n----")
     runner.test(experiment, datamodule=data, ckpt_path=args.trained_model_path)
     # Make gifs of side-by-side images
     test_output_dir = os.path.join(args.test_output_dir, exp_name) if args.side_by_side_only else os.path.join(args.test_output_dir, exp_name, 'side-by-side')
