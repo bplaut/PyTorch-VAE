c593156efd665a0f0ce2f82fe7584cf65bfe969a
Ben Plaut <bplaut@rnn.ist.berkeley.edu>
2025-03-01 06:46:52 -0800

    increased max job time since I'm gonna run with 1M training images

--- slurm_scripts/train_slurm.sh
+++ slurm_scripts/train_slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=vae
 #SBATCH --gpus=1
-#SBATCH --time=18:00:00
+#SBATCH --time=3-00:00:00
 #SBATCH --output=output_from_slurm/%j.out
 
 cd /nas/ucb/bplaut/PyTorch-VAE
