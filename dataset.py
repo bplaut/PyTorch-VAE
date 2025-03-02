bf9451f90ad4b02c3435db21c797d19286a222ca
Ben Plaut <bplaut@rnn.ist.berkeley.edu>
2025-03-01 18:30:56 -0800

    minor cleanup

--- dataset.py
+++ dataset.py
@@ -37,33 +37,7 @@ class MyDataset(Dataset):
             img = self.transform(img)
         
         return img, 0.0  # Return image and a dummy label
-
-class OxfordPets(Dataset):
-    """
-    URL = https://www.robots.ox.ac.uk/~vgg/data/pets/
-    """
-    def __init__(self, 
-                 data_path: str, 
-                 split: str,
-                 transform: Callable,
-                **kwargs):
-        self.train_data_dir = Path(data_path) / "OxfordPets"        
-        self.transforms = transform
-        imgs = sorted([f for f in self.train_data_dir.iterdir() if f.suffix == '.jpg'])
-        
-        self.imgs = imgs[:int(len(imgs) * 0.75)] if split == "train" else imgs[int(len(imgs) * 0.75):]
     
-    def __len__(self):
-        return len(self.imgs)
-    
-    def __getitem__(self, idx):
-        img = default_loader(self.imgs[idx])
-        
-        if self.transforms is not None:
-            img = self.transforms(img)
-        
-        return img, 0.0 # dummy datat to prevent breaking 
-
 class VAEDataset(LightningDataModule):
     """
     PyTorch Lightning data module 
@@ -88,7 +62,7 @@ class VAEDataset(LightningDataModule):
         patch_size: Union[int, Sequence[int]] = (256, 256),
         num_workers: int = 0,
         pin_memory: bool = False,
-        train_dataset = 'celeba',
+        train_dataset = 'coinrun',
         test_dataset = None,
         **kwargs,
     ):
