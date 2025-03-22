aa959ce0bd37bc1d4e3e89507961857dc39b6ceb
Ben Plaut <bplaut@rnn.ist.berkeley.edu>
2025-03-21 21:26:45 -0700

    minor fix

--- models/autoencoder.py
+++ models/autoencoder.py
@@ -4,7 +4,7 @@ from torch import nn
 from torch.nn import functional as F
 from torchvision.models import vgg19_bn
 from .types_ import *
-from mssim_vae import MSSIM
+from .mssim_vae import MSSIM
 
 class Autoencoder(BaseVAE):
     """
